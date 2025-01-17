524d5bd4672d8355c6eabb57ea6fd2d9c780a916
seyifunmi Odediran <philipoluseyi@gmail.com>
2025-01-17 18:23:46 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <a href="/" target="_blank">
+          <img src={viteLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
